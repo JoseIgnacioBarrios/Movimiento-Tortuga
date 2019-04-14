fc8d0f36070827fe99c84880343ea12e77a2ee0b
JoseBarrios <josebarriossc@hotmail.com>
2019-04-14 21:07:49 +0200

    Funciones Clasicas

--- p1-git.py
+++ p1-git.py
@@ -4,5 +4,11 @@ def sumatodos(limitado):
         resultado +=i
         
     return resultado
+def sumacuadrados(limitado):
+    resultado=0
+    for i in range(0,limitado+1):
+        resultado += i*i
+    return resultado
 
 print("hola",sumatodos(100))
+print (sumacuadrados(3))

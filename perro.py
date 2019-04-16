38bfcbf55f17d6c7a34a21dedd5d9d1e29b48d4d
JoseBarrios <josebarriossc@hotmail.com>
2019-04-16 18:20:13 +0200

    Primera version de Perro 16042019

--- perro.py
+++ perro.py
@@ -1,5 +1,17 @@
 class Perro():
-    def __init__(self,n,edad,peso):##nombre=n
-        self.nombre=nombre
+    def __init__(self,n,e,p):
+        self.nombre=n
         self.edad=e
-        ss
+        self.peso=p
+        
+    def ladrar(self):
+        if self.peso>=8:
+            print("Guau, Guau")
+        else:
+            print("guau,guau")
+            
+    def __str__(self):
+            return "Soy el Perro {}".format(self.nombre)
+    
+            
+            

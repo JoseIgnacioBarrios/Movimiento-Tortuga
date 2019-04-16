e07d3b2f246b3f4e14248ca3e1a39022867c8519
JoseBarrios <josebarriossc@hotmail.com>
2019-04-16 16:48:42 +0200

    objetos

---
+++ objetos.py
@@ -0,0 +1,12 @@
+import turtle
+tortuguita=turtle.Turtle()
+##funciones de 1er nivel capaces de invocarces a si
+##misma y crear compias de dsi misma
+otratortuguita=turtle.Turtle()
+
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+
+otratortuguita.color('green')
+otratortuguita.left(90)
+otratortuguita.fd(50)

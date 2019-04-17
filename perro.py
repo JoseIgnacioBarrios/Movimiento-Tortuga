5a807bbcb19bb494870a7f96e471690e3378ce9c
JoseBarrios <josebarriossc@hotmail.com>
2019-04-17 19:52:14 +0200

    Conversor Termometro

--- perro.py
+++ perro.py
@@ -1,3 +1,16 @@
+class Dog():
+    def __init__(self):
+        self.nombre=""
+        self.edad=None
+        self.peso=None
+    def ladrar (self):
+        if self.peso==None:
+            print("soy un fantasma")
+        if self.peso>=8:
+            print("GUAU , GUAU")
+        else:
+            print("ay ay")
+            
 class Perro():
     def __init__(self,n,e,p):
         self.nombre=n
@@ -11,7 +24,27 @@ class Perro():
             print("guau,guau")
             
     def __str__(self):
-            return "Soy el Perro {}".format(self.nombre)
+            return "Perro {}, e: {}, p: {}".format(self.nombre,self.edad,self.peso)
     
             
-            
+class PerroAsistencia(Perro):
+    def __init__(self,nombre,edad,peso,amo):
+        Perro.__init__(self,nombre,edad,peso)
+        self.amo=amo
+        self.__trabajando=False
+    def __str__(self):
+            return "Perro de Asistencia{}".format(self.amo)
+        
+    def Pasear(self):
+        print("{} ayudo a mi dueño, {} a pasear".format(self.nombre,self.amo))
+    def ladrar(self):
+        if self.__trabajando:
+            print("shhh , no puedo ladra")
+        else:
+            Perro.ladrar(self)
+    def trabajando(self,valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando=valor
+    
